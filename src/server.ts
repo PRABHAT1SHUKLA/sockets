3216d7495cd2ffe619fca7b838a46aa33fa55618
prabhat <prabhatshukla0849@gmail.com>
2024-10-07 08:14:39 +0530

    initialized websocket instance

--- src/server.ts
+++ src/server.ts
@@ -9,8 +9,16 @@ const port = 3000
 
 app.get("/" , (req,res) =>{
   res.send("Chat Server is Running")
-})
+});
 
 const server = app.listen(port , ()=>{
-  console.log(`Server is running on port ${port}`
+  console.log(`Server is running on port ${port}`)
+});
+
+const  wss = new WebSocketServer({server});
+
+wss.on("connection",(ws)=>{
+  console.log("Client connected");
+
+  
 })
