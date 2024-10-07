9efd77cef50210dc2a22774a4474fd1f04b6bbef
prabhat <prabhatshukla0849@gmail.com>
2024-10-07 08:12:18 +0530

    created a basic http seerver

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from "express"
+import { WebSocketServer } from "ws"
+import redisClient from "./redisClient"
+import { ChatMessage } from "./chatMessage"
+
+
+const app = express()
+const port = 3000
+
+app.get("/" , (req,res) =>{
+  res.send("Chat Server is Running")
+})
+
+const server = app.listen(port , ()=>{
+  console.log(`Server is running on port ${port}`
+})
